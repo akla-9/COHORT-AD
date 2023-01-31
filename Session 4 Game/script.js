47ae8c16a3b5dea68177c1918bd971e8bdbc703a
akla-9 <adjankla@gmail.com>
2023-01-31 16:38:59 +0000

    Added initial field validation in js

---
+++ Session 4 Game/script.js
@@ -0,0 +1,51 @@
+const form = document.querySelector(".login");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const submit = document.querySelector(".btn");
+
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    validateInputs();
+});
+
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement; 
+    const errorDisplay = inputControl.querySelector('.invalid');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('invalid');
+    inputControl.classList.remove('valid');
+}
+
+const setValid = element =>{
+    const inputControl = element.parentElement; 
+    const errorDisplay = inputControl.querySelector('.invalid');
+    errorDisplay.innerText = "";
+    inputControl.classList.add('valid');
+    inputControl.classList.remove('invalid');
+}
+
+
+const validateInputs = () =>{
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    
+    if(usernameValue ===""){
+        setError(username, 'Username is required');
+    } 
+    else {
+        setValid(username);
+    }
+
+    if(passwordValue ===""){
+        setError(password, 'Password is required');
+    } 
+    else {
+        setValid(password);
+    }
+
+    
+}
+
